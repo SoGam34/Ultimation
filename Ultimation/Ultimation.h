9765597f0e2756738c03b4121dcebbf208cc2ef0
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2023-12-08 10:24:29 +0100

    Aufraumen und löschen des Linters

--- Ultimation/Ultimation.h
+++
@@ -1,8 +0,0 @@
-﻿// Ultimation.h: Includedatei für Include-Standardsystemdateien
-// oder projektspezifische Includedateien.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
