7ea05fd48e3fc1af831973f6a54441e4aa698839
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2023-12-22 22:18:12 +0000

    Update tictactoe_field.cpp

--- tictactoe_field.cpp
+++ tictactoe_field.cpp
@@ -27,6 +27,7 @@ void Tictactoe_field::reset_field() // loops through every bracket of the field 
         }
     }
 }
+
 void Tictactoe_field::init_field() //initializes an empty tictactoe field
 {
     tictactoe_field.resize(num_col); // set the number of rows
@@ -34,66 +35,141 @@ void Tictactoe_field::init_field() //initializes an empty tictactoe field
     {
         tictactoe_field[i].resize(num_rows);
     }
+    
     reset_field();
 }
+
 mark Tictactoe_field::checkleft(std::size_t x_pos , std::size_t y_pos)
 {
-    if(x_pos == 0){return tictactoe_field[x_pos][y_pos];} // returns if no more left fields
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos-1][y_pos]){ return checkleft(x_pos-1,y_pos);} // goes left recursively
+    if(x_pos == 0)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    } // returns if no more left fields
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos-1][y_pos])
+    { 
+        return checkleft(x_pos-1,y_pos);
+    } // goes left recursively
+    
     return NULL;
 }
 
 mark Tictactoe_field::checkright(std::size_t x_pos , std::size_t y_pos)
 {
-    if(x_pos == tictactoe_field.size()-1){return tictactoe_field[x_pos][y_pos];} // returns value of field if no more left fields
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos+1][y_pos]){ return checkright(x_pos+1,y_pos);} // goes recursively to the right
+    if(x_pos == tictactoe_field.size()-1)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    } // returns value of field if no more left fields
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos+1][y_pos])
+    { 
+        return checkright(x_pos+1,y_pos);
+    } // goes recursively to the right
+    
     return NULL;
 }
 
 mark Tictactoe_field::checkup(std::size_t x_pos , std::size_t y_pos)
 {
-    if(y_pos == 0){return tictactoe_field[x_pos][y_pos];} // returns value of field if no more upper fields
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos][y_pos-1]){ return checkright(x_pos,y_pos-1);} // goes recursively up
+    if(y_pos == 0)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    } // returns value of field if no more upper fields
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos][y_pos-1])
+    { 
+        return checkright(x_pos,y_pos-1);
+    } // goes recursively up
+    
     return NULL;
 }
 
 mark Tictactoe_field::checkdown(std::size_t x_pos , std::size_t y_pos)
 {
-    if(y_pos == tictactoe_field.size()-1){return tictactoe_field[x_pos][y_pos];} // returns value of field if no more lower fields
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos][y_pos+1]){ return checkright(x_pos,y_pos+1);} // goes recursively down
+    if(y_pos == tictactoe_field.size()-1)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    } // returns value of field if no more lower fields
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos][y_pos+1])
+    {
+        return checkright(x_pos,y_pos+1);
+    } // goes recursively down
+    
     return NULL;
 }
 
 mark Tictactoe_field::checkleftup(std::size_t x_pos , std::size_t y_pos)
 {
-    if(y_pos == 0 && x_pos == 0){return tictactoe_field[x_pos][y_pos];}
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos-1][y_pos-1]){ return checkright(x_pos-1,y_pos-1);}
-    return NULL;
-}mark Tictactoe_field::checkleftdown(std::size_t x_pos , std::size_t y_pos)
-{
-    if(y_pos == tictactoe_field.size()-1 && x_pos == 0){return tictactoe_field[x_pos][y_pos];}
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos-1][y_pos+1]){ return checkright(x_pos-1,y_pos+1);}
-    return NULL;
-}mark Tictactoe_field::checkrightup(std::size_t x_pos , std::size_t y_pos)
-{
-    if(y_pos == 0 && x_pos == tictactoe_field.size()-1){return tictactoe_field[x_pos][y_pos];}
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos+1][y_pos-1]){ return checkright(x_pos+1,y_pos-1);}
-    return NULL;
-}mark Tictactoe_field::checkrightdown(std::size_t x_pos , std::size_t y_pos)
-{
-    if(y_pos == tictactoe_field.size()-1 && x_pos == tictactoe_field.size()-1){return tictactoe_field[x_pos][y_pos];}
-    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos+1][y_pos+1]){ return checkright(x_pos+1,y_pos+1);}
+    if(y_pos == 0 && x_pos == 0)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    }
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos-1][y_pos-1])
+    { 
+        return checkright(x_pos-1,y_pos-1);
+    }
+    
     return NULL;
 }
+
+mark Tictactoe_field::checkleftdown(std::size_t x_pos , std::size_t y_pos)
+{
+    if(y_pos == tictactoe_field.size()-1 && x_pos == 0)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    }
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos-1][y_pos+1])
+    { 
+        return checkright(x_pos-1,y_pos+1);
+    }
+    
+    return NULL;
+}
+
+mark Tictactoe_field::checkrightup(std::size_t x_pos , std::size_t y_pos)
+{
+    if(y_pos == 0 && x_pos == tictactoe_field.size()-1)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    }
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos+1][y_pos-1])
+    { 
+        return checkright(x_pos+1,y_pos-1);
+    }
+    
+    return NULL;
+}
+
+mark Tictactoe_field::checkrightdown(std::size_t x_pos , std::size_t y_pos)
+{
+    if(y_pos == tictactoe_field.size()-1 && x_pos == tictactoe_field.size()-1)
+    {
+        return tictactoe_field[x_pos][y_pos];
+    }
+    
+    if(tictactoe_field[x_pos][y_pos] == tictactoe_field[x_pos+1][y_pos+1])
+    { 
+        return checkright(x_pos+1,y_pos+1);
+    }
+    
+    return NULL;
+}
+
 mark Tictactoe_field::checkrow(std::size_t x_pos , std::size_t y_pos)
 {
     mark result_checkup = checkup(x_pos , y_pos);
     if(result_checkup == NULL){
      return NULL;
     }
+    
     if(result_checkup == checkdown(x_pos , y_pos)){
     return result_checkup;
     }
+    
     return NULL;
 }
 mark Tictactoe_field::checkcol(std::size_t x_pos , std::size_t y_pos)
@@ -102,9 +178,11 @@ mark Tictactoe_field::checkcol(std::size_t x_pos , std::size_t y_pos)
     if(result_checkleft == NULL){
         return NULL;
     }
+    
     if(result_checkleft == checkright(x_pos , y_pos)){
     return result_checkleft;
     }
+    
     return NULL;
 }
 mark Tictactoe_field::checkdiag(std::size_t x_pos , std::size_t y_pos)
@@ -113,9 +191,11 @@ mark Tictactoe_field::checkdiag(std::size_t x_pos , std::size_t y_pos)
     if (result_checkleftup == NULL){
         return NULL;
     }
+    
     if(result_checkleftup == checkrightdown(x_pos , y_pos)){
         return result_checkleftup;
     }
+    
     mark result_checkleftdown = checkleftdown(x_pos , y_pos);
     if(result_checkleftdown == NULL){
         return NULL;
@@ -123,6 +203,7 @@ mark Tictactoe_field::checkdiag(std::size_t x_pos , std::size_t y_pos)
     if(result_checkleftdown == checkrightup(x_pos , y_pos)){
         return result_checkleftdown;
     }
+    
         return NULL;
 }
 mark Tictactoe_field::checkwinn(std::size_t x_pos , std::size_t y_pos)
@@ -131,10 +212,12 @@ mark Tictactoe_field::checkwinn(std::size_t x_pos , std::size_t y_pos)
     if (result != NULL){
         return result;
     }
+    
     result = checkrow(x_pos , y_pos);
     if(result != NULL){
         return result;
     }
+    
     return checkdiag(x_pos , y_pos);
 }
 
