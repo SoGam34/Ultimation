acb247dcd2dc22e8f4e83ca88854330225d4c1ff
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2024-01-15 13:02:06 +0100

    Namen überarbeiten

--- main.cpp
+++ main.cpp
@@ -5,57 +5,57 @@
 
 class SQLAbfragen
 {
-    std::string to_string(std::vector<std::vector<char>>& map, std::string& finaler_string, int zeile, int spalte)
+    std::string to_string(std::vector<std::vector<char>>& Spielfeld, std::string& finaler_string, int zeile, int spalte)
     {
-        if(map.size()-1 == zeile && map[zeile].size()-1 == spalte)
+        if(Spielfeld.size()-1 == zeile && Spielfeld[zeile].size()-1 == spalte)
         {
-            finaler_string.push_back(map[zeile][spalte]);
+            finaler_string.push_back(Spielfeld[zeile][spalte]);
             return finaler_string;
         }
 
-        if(map[zeile].size()-1 == spalte)
+        if(Spielfeld[zeile].size()-1 == spalte)
         {
-            finaler_string.push_back(map[zeile][spalte]);
+            finaler_string.push_back(Spielfeld[zeile][spalte]);
             finaler_string.push_back('U');
-            return to_string(map, finaler_string, zeile+1, 0);
+            return to_string(Spielfeld, finaler_string, zeile+1, 0);
         }
 
-        finaler_string.push_back(map[zeile][spalte]);
-        return to_string(map,finaler_string , zeile, spalte+1);
+        finaler_string.push_back(Spielfeld[zeile][spalte]);
+        return to_string(Spielfeld,finaler_string , zeile, spalte+1);
     }
 
-    std::vector<std::vector<char>> to_vector(std::string& map, std::vector<std::vector<char>>& finaler_vector, size_t zeile)
+    std::vector<std::vector<char>> to_vector(std::string& Spielfeld, std::vector<std::vector<char>>& finaler_vector, size_t zeile)
     {
-        if(map.empty())
+        if(Spielfeld.empty())
         {
             return finaler_vector;
         }
 
-        if(map.front() == 'U')
+        if(Spielfeld.front() == 'U')
         {
-            map.erase(map.begin()); // entfernen von U
-            finaler_vector.push_back({map.front()}); //erstellen der neuen Zeile im Vector
-            map.erase(map.begin());        //entfernen des ersten Zeichens
-            return to_vector(map, finaler_vector, zeile+1);
+            Spielfeld.erase(Spielfeld.begin()); // entfernen von U
+            finaler_vector.push_back({Spielfeld.front()}); //erstellen der neuen Zeile im Vector
+            Spielfeld.erase(Spielfeld.begin());        //entfernen des ersten Zeichens
+            return to_vector(Spielfeld, finaler_vector, zeile+1);
         }
 
-        finaler_vector[zeile].push_back(map.front());
-        map.erase(map.begin());
-        return to_vector(map, finaler_vector, zeile);
+        finaler_vector[zeile].push_back(Spielfeld.front());
+        Spielfeld.erase(Spielfeld.begin());
+        return to_vector(Spielfeld, finaler_vector, zeile);
     }
 public:
     std::vector<std::vector<char>> getSpielfeld(int ID)
     {
-        std::vector<std::vector<char>> map{{}};
-        std::string s = "fffUfffUfff"; //Normallerweise wird hier der String aus der Datenbank geholt
+        std::vector<std::vector<char>> Spielfeld{{}};
+        std::string s{"fffUfffUfff"}; //Normallerweise wird hier der String aus der Datenbank geholt
 
-        return to_vector(s, map, 0);
+        return to_vector(s, Spielfeld, 0);
     }
 
-    void setSpielfeld(int ID, std::vector<std::vector<char>> map)
+    void setSpielfeld(int ID, std::vector<std::vector<char>> Spielfeld)
     {
         std::string s2 ="";// Damit wir eine Speicheradresse haben
-        s2 = to_string(map, s2, 0,0);
+        s2 = to_string(Spielfeld, s2, 0,0);
 
         //TODO: hier in der Datenbank speichern
     }
@@ -63,13 +63,13 @@ public:
 
 int main()
 {
-    std::unique_ptr<SQLAbfragen> db(new SQLAbfragen());
+    std::unique_ptr<SQLAbfragen> db{std::make_unique_ptr<SQLAbfragen>()};
 
-    std::vector<std::vector<char>> map = db->getSpielfeld(1);
+    std::vector<std::vector<char>> Spielfeld{db->getSpielfeld(1)};
 
-    map.push_back({'f', 'x'});
+    Spielfeld.push_back({'f', 'x'});
 
-    db->setSpielfeld(1, map);
+    db->setSpielfeld(1, Spielfeld);
 
     return 0;
 }
