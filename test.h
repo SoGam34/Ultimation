13dd75f76a9c85e9f7e74458c528ee5c90db736b
ProTayToe <149896703+FabianHaag474@users.noreply.github.com>
2024-01-19 10:02:00 +0100

    to_2Dvector fix
    fixed an issue, where the row of the vector wouldnt be increases properly

--- test.h
+++ test.h
@@ -32,42 +32,43 @@ Testclass::~Testclass()
 }
 
 std::string Testclass::to_string(std::vector<std::vector<char>> field_vector, std::string field_string = ""){
-   if(field_vector.empty()){
-    return field_string;
-   } 
-   else{
-    field_string.append(std::to_string(field_vector.front().front()));
-    field_vector.front().erase(field_vector.front().begin());
-    if(field_vector.front().empty()){
-        field_vector.erase(field_vector.begin());
+    if(field_vector.empty()){
+        return field_string;
     }
-    return to_string(field_vector,field_string);
-   }
+    else{
+        field_string.append(std::to_string(field_vector.front().front()));
+        field_vector.front().erase(field_vector.front().begin());
+        if(field_vector.front().empty()){
+            field_vector.erase(field_vector.begin());
+        }
+        return to_string(field_vector,field_string);
+    }
 }
 
 
 std::vector<std::vector<char>> Testclass::to_2Dvector(std::string field_string){
-    std::vector<std::vector<char>> field_vector;
+    std::vector<std::vector<char>> field_vector{{}};
     return to_2Dvector(field_vector,field_string,0);
 }
 
 std::vector<std::vector<char>> Testclass::to_2Dvector(std::vector<std::vector<char>> field_vector, std::string field_string,size_t row = 0){
 
     if(field_string.empty()){return field_vector;}
-    if(field_string[0] == '|'){
-            field_string.erase(0);// pops '|' 
-            field_vector[row].push_back(field_string[0]); //adds new character to the current row
-            field_string.erase(0); // pops the inserted character
-            return to_2Dvector(field_vector,field_string,row); 
+
+    else if(field_string[0] == '|'){
+        field_string.erase(0,1);// pops '|'
+        field_vector[row].push_back(field_string[0]); //adds new character to the current row
+        field_string.erase(0,1); // pops the inserted character
+        return to_2Dvector(field_vector,field_string,row);
     }
 
-        else if(field_string[0] == '/'){// pop first character and add a new row to the vector and insert the first character
-            field_string.erase(0);// pops '/'
-            field_vector[row].push_back(field_string[0]); // adds a new  row and fills in the first character 
-            field_string.erase(0); // pops the inserted character
-            field_vector.push_back({});
-            return to_2Dvector(field_vector,field_string,row++);
+    else if(field_string[0] == '/'){// pop first character and add a new row to the vector and insert the first character
+        field_string.erase(0,1);// pops '/'
+        field_vector.push_back({field_string[0]}); //appends character in new row
+        field_string.erase(0,1); // pops the inserted character
+        return to_2Dvector(field_vector,field_string,row++);
     }
+    else{return {{}};}
 }
 
 void Testclass::print_string_field(){
