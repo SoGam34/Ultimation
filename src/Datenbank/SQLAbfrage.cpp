0acea91cca67ffb09c5c92e83cd1466fb792c6b8
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2024-02-09 09:26:43 +0100

    Anpassen von SQLAbfrage.cpp
    Die Funktionen die in der .cpp Datein breits implementiert waren stimmen nun mit den Definitionen in der .hpp Datei über ein.

--- src/Datenbank/SQLAbfrage.cpp
+++ src/Datenbank/SQLAbfrage.cpp
@@ -1,58 +1,57 @@
 #include "SQLAbfrage.hpp"
 
-
-std::string SQLAbfrage::to_string(std::vector<std::vector<char>>& Spielfeld, std::string& finaler_string, int zeile, int spalte) 
-{// soll das Spielfeld von einem 2-Dimensionalen Vektor in einen std::string nach Schema "fffufffufff" umwandeln. Die Zeitkomplexität ist O(n²).   
-    if(Spielfeld.size()-1 == zeile && Spielfeld[zeile].size()-1 == spalte) // fügt, wenn im letzten feld, dieses an den std::string an und gibt diesen zurück
+std::string SQLAbfrage::to_string(std::vector<std::vector<char>>& Field, std::string& final_string, size_t zeile, size_t spalte) 
+{// soll das Field von einem 2-Dimensionalen Vektor in einen std::string nach Schema "fffufffufff" umwandeln. Die Zeitkomplexität ist O(n²).   
+    if(Field.size()-1 == zeile && Field[zeile].size()-1 == spalte) // fügt, wenn im letzten feld, dieses an den std::string an und gibt diesen zurück
     {
-        finaler_string.push_back(Spielfeld[zeile][spalte]);
-        return finaler_string;
+        final_string.push_back(Field[zeile][spalte]);
+        return final_string;
     }
 
-    if(Spielfeld[zeile].size()-1 == spalte) // wenn eine zeile zuende ist, wird auch ein 'U' an den std::string angehängt, um dies anzuzeigen
+    if(Field[zeile].size()-1 == spalte) // wenn eine zeile zuende ist, wird auch ein 'U' an den std::string angehängt, um dies anzuzeigen
     {
-        finaler_string.push_back(Spielfeld[zeile][spalte]); // zeichen wird an std::string angehängt
-        finaler_string.push_back('U'); //signalisiert neue Zeile
-        return to_string(Spielfeld, finaler_string, zeile+1, 0); // setzt die Spalte zurück und inkrementiert die Zeile
+        final_string.push_back(Field[zeile][spalte]); // zeichen wird an std::string angehängt
+        final_string.push_back('U'); //signalisiert neue Zeile
+        return to_string(Field, final_string, zeile+1, 0); // setzt die Spalte zurück und inkrementiert die Zeile
     }
 
     else
     { // bei felder, welche nicht am Ende der Zeile sind, wird das Element angehängt und die Spalte inkrementiert
-        finaler_string.push_back(Spielfeld[zeile][spalte]);
-        return to_string(Spielfeld,finaler_string , zeile, spalte+1);
+        final_string.push_back(Field[zeile][spalte]);
+        return to_string(Field, final_string, zeile, spalte+1);
     }
 }
 
-std::vector<std::vector<char>> SQLAbfrage::to_vector(std::string& Spielfeld, std::vector<std::vector<char>>& finaler_vector, size_t zeile)
+std::vector<std::vector<char>> SQLAbfrage::to_vector(std::string& Field, std::vector<std::vector<char>>& final_vector, size_t zeile)
 {
-    if(Spielfeld.empty())
+    if(Field.empty())
     {
-        return finaler_vector;
+        return final_vector;
     }
 
-    if(Spielfeld.front() == 'U')
+    if(Field.front() == 'U')
     {
-        Spielfeld.erase(Spielfeld.begin()); // entfernen von U
-        finaler_vector.push_back({Spielfeld.front()}); //erstellen der neuen Zeile im Vector
-        Spielfeld.erase(Spielfeld.begin());        //entfernen des ersten Zeichens
-        return to_vector(Spielfeld, finaler_vector, zeile+1);
+        Field.erase(Field.begin()); // entfernen von U
+        final_vector.push_back({Field.front()}); //erstellen der neuen Zeile im Vector
+        Field.erase(Field.begin());        //entfernen des ersten Zeichens
+        return to_vector(Field, final_vector, zeile+1);
     }
 
-    finaler_vector[zeile].push_back(Spielfeld.front());
-    Spielfeld.erase(Spielfeld.begin());
-    return to_vector(Spielfeld, finaler_vector, zeile);
+    final_vector[zeile].push_back(Field.front());
+    Field.erase(Field.begin());
+    return to_vector(Field, final_vector, zeile);
 }
 
-std::vector<std::vector<char>> SQLAbfrage::getSpielfeld(int ID)
+std::vector<std::vector<char>> SQLAbfrage::getField(int FieldID)
 {
     std::vector<std::vector<char>> Spielfeld{{}};
     std::string s{"fffUfffUfff"}; //Normallerweise wird hier der String aus der Datenbank geholt
     return to_vector(s, Spielfeld, 0);
 }
 
-void SQLAbfrage::setSpielfeld(int ID, std::vector<std::vector<char>> Spielfeld)
+void SQLAbfrage::setField(int FieldID, std::vector<std::vector<char>> Field)
 {
-    std::string s2 ="";// Damit wir eine Speicheradresse haben
-    s2 = to_string(Spielfeld, s2, 0,0);
+    std::string s ="";// Damit wir eine Speicheradresse haben
+    s = to_string(Field, s, 0, 0);
     //TODO: hier in der Datenbank speichern
 }
